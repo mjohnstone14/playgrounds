84615eccda50d181ad55c879d532a203044efc9e
Marwan Johnstone <mjohnstone@eaglerockschool.org>
2023-08-14 11:35:17 -0600

    Add mantine

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,38 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { MantineProvider } from '@mantine/core'
+import { DashHeader } from './components/DashHeader'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <MantineProvider 
+      withGlobalStyles 
+      withNormalizeCSS
+      theme={{
+        colorScheme: 'light',
+        colors: {
+          // Add your color
+          deepBlue: ['#E9EDFC', '#C1CCF6', '#99ABF0' /* ... */],
+          // or replace default theme color
+          blue: ['#E9EDFC', '#C1CCF6', '#99ABF0' /* ... */],
+        },
+
+        shadows: {
+          md: '1px 1px 3px rgba(0, 0, 0, .25)',
+          xl: '5px 5px 3px rgba(0, 0, 0, .25)',
+        },
+
+        headings: {
+          fontFamily: 'Roboto, sans-serif',
+          sizes: {
+            h1: { fontSize: '2rem' },
+          },
+        },
+      }}
+    >
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -21,6 +47,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <DashHeader />
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -28,7 +55,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </MantineProvider>
   )
 }
 
